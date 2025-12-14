901961de929b0cde21301e44c2d0b0a60fd20ff1
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2025-12-14 01:20:23 +0000

    build: finalizando a configuração do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,41 @@ module.exports = {
     root: true,
     env:{
         browser: true,
-        node:true
+        node:true,
+        es2021: true
     },
     ignorePatterns: ["node_modules/","build/"],
+    extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
+    parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+    rules: {
+    "no-unused-vars": "warn",
+        "no-undef": "error",
+    "no-console": "warn",
+    "no-debugger": "warn",
+
+    "max-len": ["warn", { code: 100 }],
+    "semi": ["error", "never"],
+    "quotes": ["error", "single"],
+},
+  overrides: [
+    {
+      files: ["api/**/*"],
+      env: { node: true },
+    },
+    {
+      files: ["web/**/*"],
+      env: { browser: true },
+    },
+    {
+      files: ["shared/**/*"],
+      env: { browser: true, node: true },
+    }
+  ],
+
 }
