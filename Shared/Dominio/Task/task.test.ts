4bc8885ea60bef866be83016a74cfb142e2a7ecf
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2025-12-16 20:23:24 +0000

    feat(task): adicionando metodos set da entidade task e seus testes

--- Shared/Dominio/Task/task.test.ts
+++ Shared/Dominio/Task/task.test.ts
@@ -1,38 +1,92 @@
-import {Task} from "./task"
+import { Task } from './task'
 
-describe("Task",()=>{
-    it("Deve criar uma task com as propriedades corretas",()=>{
-        const dataCriacao = new Date("2024-06-01");
-        const task = new Task({
-            id: 1,
-            titulo: "Estudar TypeScript",
-            descricao: "Ler a documentação oficial do TypeScript",
-            taskStatus: "Pendente",
-            dataCriacao: dataCriacao
-        });
-
-        expect(task.getId()).toBe(1);
-        expect(task.getTitulo()).toBe("Estudar TypeScript");
-        expect(task.getDescricao()).toBe("Ler a documentação oficial do TypeScript");
-        expect(task.getTaskStatus()).toBe("Pendente");
-        expect(task.getDataCriacao()).toBe(dataCriacao);
+describe('Task', () => {
+  it('Deve criar uma task com as propriedades corretas', () => {
+    const dataCriacao = new Date('2024-06-01')
+    const task = new Task({
+      id: 1,
+      titulo: 'Estudar TypeScript',
+      descricao: 'Ler a documentação oficial do TypeScript',
+      dataCriacao: dataCriacao,
+      taskStatus: 'Pendente',
     })
 
-    it("Deve criar uma task com status padrão 'Pendente' e data de criação atual",()=>{
-        const dataAntiga = new Date();
-        const task = new Task({
-            id: 2,
-            titulo: "Fazer exercícios",
-            descricao: "Resolver problemas de programação"
-        });
-        const dataNova = new Date();
-        
+    expect(task.getId()).toBe(1)
+    expect(task.getTitulo()).toBe('Estudar TypeScript')
+    expect(task.getDescricao()).toBe('Ler a documentação oficial do TypeScript')
+    expect(task.getTaskStatus()).toBe('Pendente')
+    expect(task.getDataCriacao()).toBe(dataCriacao)
+  })
 
-        expect(task.getId()).toBe(2);
-        expect(task.getTitulo()).toBe("Fazer exercícios");
-        expect(task.getDescricao()).toBe("Resolver problemas de programação");
-        expect(task.getTaskStatus()).toBe("Pendente");
-        expect(task.getDataCriacao().getTime()).toBeGreaterThanOrEqual(dataNova.getTime());
-        expect(task.getDataCriacao().getTime()).toBeLessThanOrEqual(dataAntiga.getTime());
+  it('Deve criar uma task com status padrão Pendente e data de criação atual', () => {
+    const dataAntiga = new Date()
+    const task = new Task({
+      id: 2,
+      titulo: 'Fazer exercícios',
+      descricao: 'Resolver problemas de programação',
     })
+    const dataNova = new Date()
+
+    expect(task.getId()).toBe(2)
+    expect(task.getTitulo()).toBe('Fazer exercícios')
+    expect(task.getDescricao()).toBe('Resolver problemas de programação')
+    expect(task.getTaskStatus()).toBe('Pendente')
+    expect(task.getDataCriacao().getTime()).toBeGreaterThanOrEqual(dataNova.getTime())
+    expect(task.getDataCriacao().getTime()).toBeLessThanOrEqual(dataAntiga.getTime())
+  })
+
+  it('Deve permitir alterar o status de pendente para em andamento', () => {
+    const dataCriacao = new Date('2020-06-12')
+    const task = new Task({
+      id: 3,
+      titulo: 'estudando inglês',
+      descricao: 'chegar no nível C1',
+      dataCriacao: dataCriacao,
+    })
+
+    expect(task.getTaskStatus()).toBe('Pendente')
+    task.setStatusEmAndamento()
+    expect(task.getId()).toBe(3)
+    expect(task.getTitulo()).toBe('estudando inglês')
+    expect(task.getDescricao()).toBe('chegar no nível C1')
+    expect(task.getTaskStatus()).toBe('Em Andamento')
+    expect(task.getDataCriacao()).toBe(dataCriacao)
+  })
+
+  it('Deve permitir alterar o status de em andamento para concluída', () => {
+    const dataCriacao = new Date('2020-06-12')
+    const task = new Task({
+      id: 3,
+      titulo: 'estudando inglês',
+      descricao: 'chegar no nível C1',
+      taskStatus: 'Em Andamento',
+      dataCriacao: dataCriacao,
+    })
+
+    expect(task.getTaskStatus()).toBe('Em Andamento')
+    task.setStatusConcluido()
+    expect(task.getId()).toBe(3)
+    expect(task.getTitulo()).toBe('estudando inglês')
+    expect(task.getDescricao()).toBe('chegar no nível C1')
+    expect(task.getTaskStatus()).toBe('Concluída')
+    expect(task.getDataCriacao()).toBe(dataCriacao)
+  })
+
+  it('Deve permitir alterar o titulo da tarefa', () => {
+    const dataCriacao = new Date('2020-06-12')
+    const task = new Task({
+      id: 3,
+      titulo: 'estudando inglês',
+      descricao: 'chegar no nível C1',
+      taskStatus: 'Em Andamento',
+      dataCriacao: dataCriacao,
+    })
+    expect(task.getTitulo()).toBe('estudando inglês')
+    task.alteraTituloTask('estudando espanhol')
+    expect(task.getId()).toBe(3)
+    expect(task.getTitulo()).toBe('estudando espanhol')
+    expect(task.getDescricao()).toBe('chegar no nível C1')
+    expect(task.getTaskStatus()).toBe('Em Andamento')
+    expect(task.getDataCriacao()).toBe(dataCriacao)
+  })
 })
--- Shared/Dominio/Task/task.ts
+++ Shared/Dominio/Task/task.ts
@@ -1,49 +1,69 @@
-export type TaskStatus = "Pendente" | "Em Andamento" | "Concluída";
+export type TaskStatus = 'Pendente' | 'Em Andamento' | 'Concluída'
 
-interface TaskPropriedades{
-    id: number;
-    titulo: string;
-    descricao: string;
-    taskStatus?: TaskStatus;
-    dataCriacao?: Date;
+export interface TaskPropriedades {
+  id: number
+  titulo: string
+  descricao: string
+  taskStatus?: TaskStatus
+  dataCriacao?: Date
 }
 
-export class Task{
-    private id: number;
-    private titulo: string;
-    private descricao: string;
-    private taskStatus: TaskStatus;
-    private dataCriacao: Date;
+export class Task {
+  private id: number
+  private titulo: string
+  private descricao: string
+  private taskStatus: TaskStatus
+  private dataCriacao: Date
 
-    constructor(props: TaskPropriedades){
-        this.id = props.id;
-        this.titulo = props.titulo;
-        this.descricao = props.descricao;
-        this.taskStatus = props.taskStatus ?? "Pendente";
-        this.dataCriacao = props.dataCriacao ?? new Date();
+  constructor(props: TaskPropriedades) {
+    this.id = props.id
+    this.titulo = props.titulo
+    this.descricao = props.descricao
+    this.taskStatus = props.taskStatus ?? 'Pendente'
+    this.dataCriacao = props.dataCriacao ?? new Date()
+  }
+
+  public getId(): number {
+    return this.id
+  }
+
+  public getTitulo(): string {
+    return this.titulo
+  }
+
+  public getDescricao(): string {
+    return this.descricao
+  }
+
+  public getTaskStatus(): TaskStatus {
+    return this.taskStatus
+  }
+
+  public getDataCriacao(): Date {
+    return this.dataCriacao
+  }
+
+  public setStatusEmAndamento(): void {
+    if (this.taskStatus != 'Pendente') {
+      throw new Error('Apenas tarefas pendentes podem ser iniciadas')
     }
 
-    public getId():number{
-        return this.id;
+    this.taskStatus = 'Em Andamento'
+  }
+
+  public setStatusConcluido(): void {
+    if (this.taskStatus != 'Em Andamento') {
+      throw new Error('Apenas tarefas em andamento podem ser concluidas')
     }
 
-    public getTitulo():string{
-        return this.titulo;
+    this.taskStatus = 'Concluída'
+  }
+
+  public alteraTituloTask(novoTitulo: string): void {
+    if (novoTitulo.length < 3) {
+      throw new Error('Apenas titulos com mais de 3 letras podem ser criados')
     }
 
-    public getDescricao():string{
-        return this.descricao;
-    }
-
-    public getTaskStatus():TaskStatus{
-        return this.taskStatus;
-    }
-
-    public getDataCriacao():Date{
-        return this.dataCriacao;
-    }
-
-    
-
-
+    this.titulo = novoTitulo
+  }
 }
