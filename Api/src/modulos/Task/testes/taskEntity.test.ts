95dee41111c96fedea33f32482fd8468667383cc
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-01-20 21:24:55 +0000

    test(taskEntity): alterando mensagem de erro

--- Api/src/modulos/Task/testes/taskEntity.test.ts
+++ Api/src/modulos/Task/testes/taskEntity.test.ts
@@ -182,7 +182,7 @@ describe('Task', () => {
 
     expect(task.getId()).toBe(11)
     expect(task.getTitulo()).toBe('Estudar Go')
-    expect(() => task.alteraTituloTask('ab')).toThrow('Titulo tem que ter pelo menos 3 caracteres')
+    expect(() => task.alteraTituloTask('ab')).toThrow('Titulo tem que ter no minimo 3 caracteres')
     expect(task.getTitulo()).toBe('Estudar Go')
     expect(task.getTaskStatus()).toBe(TaskStatus.EmAndamento)
     expect(task.getDataCriacao()).toBe(dataCriacao)
