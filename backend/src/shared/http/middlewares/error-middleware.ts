1cc75d41865f74ca585e3c066a7e1f6325e256bf
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-02-18 01:01:44 +0000

    chore(error-middleware): alterando o formato com prittier e eslint

--- backend/src/shared/http/middlewares/error-middleware.ts
+++ backend/src/shared/http/middlewares/error-middleware.ts
@@ -1,9 +1,14 @@
-import { Request, Response, NextFunction, application } from "express";
-import { ApplicationError } from "../../errors/errors";
-export function errorHandler(error: unknown, request: Request, response: Response, nextFunction: NextFunction){
-    if(error instanceof ApplicationError ){
-        return response.json({Error: error.message } ).status(error.statusCode)
-    }
+import { Request, Response, NextFunction, application } from 'express'
+import { ApplicationError } from '../../errors/errors'
+export function errorHandler(
+  error: unknown,
+  request: Request,
+  response: Response,
+  nextFunction: NextFunction
+) {
+  if (error instanceof ApplicationError) {
+    return response.json({ Error: error.message }).status(error.statusCode)
+  }
 
-    return response.json('Internal server error').status(500)
+  return response.json('Internal server error').status(500)
 }
