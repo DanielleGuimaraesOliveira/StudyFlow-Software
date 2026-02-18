d538906d53642baf5d68c093dd1eae8fe2f2eba6
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-02-18 00:55:05 +0000

    feat(error-middleware): adicionando o middleware de erros globais para o servidor

---
+++ backend/src/shared/http/middlewares/error-middleware.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction, application } from "express";
+import { ApplicationError } from "../../errors/errors";
+export function errorHandler(error: unknown, request: Request, response: Response, nextFunction: NextFunction){
+    if(error instanceof ApplicationError ){
+        return response.json({Error: error.message } ).status(error.statusCode)
+    }
+
+    return response.json('Internal server error').status(500)
+}
