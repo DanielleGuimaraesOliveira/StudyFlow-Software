76c5a7925eaeab76c0f1359279ca1ab3310fc5dd
Danielle Guimarães <157411604+DanielleGuimaraesOliveira@users.noreply.github.com>
2026-02-20 03:24:38 +0000

    chore(serve): atualizando o serve e modularizando elementos dele

--- backend/src/serve.ts
+++ backend/src/serve.ts
@@ -1,19 +1,8 @@
 import 'dotenv/config'
 import express from 'express'
-import { z } from 'zod'
 import cors from 'cors'
-import { TaskController } from './interfaces/http/task/task-controller'
-import { TaskService } from './application/task/task-service'
-import { TaskRepositoryPg } from './domain/task/task-repository'
-import { validation } from './shared/http/middlewares/validation-middleware'
-import {
-  taskStatusParamsSchema,
-  taskIdParamsSchema,
-  changeDescriptionTaskSchema,
-  changeTitleTaskSchema,
-  criarTaskSchema,
-} from './interfaces/http/task/task-schema'
 import { errorHandler } from './shared/http/middlewares/error-middleware'
+import taskRouter from './interfaces/http/task/task-router'
 
 const app = express()
 
@@ -26,47 +15,7 @@ app.use(
   })
 )
 
-const repository = new TaskRepositoryPg()
-const service = new TaskService(repository)
-const taskController = new TaskController(service)
-
-app.post('/tasks', validation(criarTaskSchema), (req, res) => taskController.registerTask(req, res))
-
-app.get(
-  '/tasks/status/:taskStatus',
-  validation(z.object({ params: taskStatusParamsSchema })),
-  (req, res) => taskController.showByStatus(req, res)
-)
-
-app.get('/tasks', (req, res) => taskController.showAllTasks(req, res))
-
-app.get('/tasks/:id', validation(z.object({ params: taskIdParamsSchema })), (req, res) =>
-  taskController.findById(req, res)
-)
-
-app.put('/tasks/:id/inicia', validation(z.object({ params: taskIdParamsSchema })), (req, res) =>
-  taskController.startTask(req, res)
-)
-
-app.put('/tasks/:id/conclui', validation(z.object({ params: taskIdParamsSchema })), (req, res) =>
-  taskController.doneTask(req, res)
-)
-
-app.put(
-  '/tasks/:id/titulo/:titulo',
-  validation(z.object({ params: changeTitleTaskSchema })),
-  (req, res) => taskController.changeTitle(req, res)
-)
-
-app.put(
-  '/tasks/:id/descricao/:descricao',
-  validation(z.object({ params: changeDescriptionTaskSchema })),
-  (req, res) => taskController.changeDescription(req, res)
-)
-
-app.delete('/tasks/:id', validation(z.object({ params: taskIdParamsSchema })), (req, res) =>
-  taskController.deleteTask(req, res)
-)
+app.use('/tasks', taskRouter)
 
 app.use(errorHandler)
 
