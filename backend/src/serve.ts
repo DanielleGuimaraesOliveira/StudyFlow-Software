a7322371cd14afdccdbb3fccf047e4bd186aa568
Danielle Guimarães <danielleguimaraes@tecgraf.puc-rio.br>
2026-02-25 01:01:59 -0300

    chore(serve): retirando comentario

--- backend/src/serve.ts
+++ backend/src/serve.ts
@@ -19,7 +19,6 @@ app.use('/tasks', taskRouter)
 
 app.use(errorHandler)
 
-// Só inicia o servidor se for executado diretamente, não quando importado para testes
 if (require.main === module) {
   app.listen(3000, () => {
     // eslint-disable-next-line no-console
